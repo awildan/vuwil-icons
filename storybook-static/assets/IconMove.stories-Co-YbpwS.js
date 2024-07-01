02f2618f1a28d35d076e85050badff868077fa55
awil <mdl.awildanfirmansyah@gmail.com>
2024-07-01 17:39:20 +0700

    add static page

---
+++ storybook-static/assets/IconMove.stories-Co-YbpwS.js
@@ -0,0 +1,21 @@
+import{b as p,d as g,o as f}from"./vue.esm-bundler-BbkTqTUQ.js";import{_ as h}from"./_plugin-vue_export-helper-DlAUqK2U.js";const i={name:"IconMove",props:{size:{type:[String,Number],default:24},width:{type:[String,Number],default:""},height:{type:[String,Number],default:""},color:{type:String,default:"#A4A4A4"}}},v=["width","height"],_=["fill"];function b(t,I,e,w,M,y){return f(),p("svg",{viewBox:"0 0 24 24",width:e.width||e.size,height:e.height||e.size,xmlns:"http://www.w3.org/2000/svg"},[g("path",{d:"M13 11V6h3l-4-4-4 4h3v5H6V8l-4 4 4 4v-3h5v5H8l4 4 4-4h-3v-5h5v3l4-4-4-4v3h-5z",fill:e.color},null,8,_)],8,v)}const m=h(i,[["render",b]]);i.__docgenInfo={displayName:"IconMove",exportName:"default",description:"",tags:{},props:[{name:"size",type:{name:"string|number"},defaultValue:{func:!1,value:"24"}},{name:"width",type:{name:"string|number"},defaultValue:{func:!1,value:'""'}},{name:"height",type:{name:"string|number"},defaultValue:{func:!1,value:'""'}},{name:"color",type:{name:"string"},defaultValue:{func:!1,value:'"#A4A4A4"'}}],sourceFiles:["/Users/macbookpro/Documents/Work/Personal/Repo/vuwil-icons/src/icons/IconMove.vue"]};const z={title:"Icons/IconMove",component:m,tags:["autodocs"],argTypes:{size:{control:"number"},width:{control:"number"},height:{control:"number"},color:{control:"color"}}},d=t=>({components:{IconMove:m},setup(){return{args:t}},template:'<IconMove v-bind="args" />'}),o=d.bind({});o.args={size:40,color:"#A4A4A4"};const n=d.bind({});n.args={color:"#551cbf"};var r,s,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`args => ({
+  components: {
+    IconMove
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<IconMove v-bind="args" />\`
+})`,...(a=(s=o.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var c,l,u;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`args => ({
+  components: {
+    IconMove
+  },
+  setup() {
+    return {
+      args
+    };
+  },
+  template: \`<IconMove v-bind="args" />\`
+})`,...(u=(l=n.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const V=["Default","CustomColors"];export{n as CustomColors,o as Default,V as __namedExportsOrder,z as default};
